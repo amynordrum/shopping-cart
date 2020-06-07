ef020e6920408a4757bfab5bec946d0bffd80150
amynordrum <amynordrum@users.noreply.github.com>
2020-06-07 12:45:17 -0400

    Keeps running total of price

--- shopping_cart.py
+++ shopping_cart.py
@@ -37,6 +37,8 @@ def to_usd(my_price):
 
 # TODO: write some Python code here to produce the desired output
 
+total_price = 0
+
 while True:
     selected_id = input("Please scan the next item: ")
     if selected_id == "DONE":
@@ -44,5 +46,7 @@ while True:
     else:
         matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
         matching_product = matching_products[0]
+        total_price = total_price + matching_product["price"]
         print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
 
+print("TOTAL PRICE: " + str(total_price))
