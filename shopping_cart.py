5a3e3b658d80e3e1b0f9dedd3b83ade6e90f124a
amynordrum <amynordrum@users.noreply.github.com>
2020-06-07 13:15:53 -0400

    Added pleasant messages

--- shopping_cart.py
+++ shopping_cart.py
@@ -49,16 +49,21 @@ while True:
 
 
 # print(selected_ids)
-
-# print("TOTAL PRICE: " + str(total_price))
+print("------------------")
+print("SEVENTH STREET GROCER")
+print("www.seventhstreetgrocer.com")
+print("------------------")
 
 for selected_id in selected_ids:
     matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
     matching_product = matching_products[0]
     total_price = total_price + matching_product["price"]
-    print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
+    print(matching_product["name"] + " " + str(matching_product["price"]))
     
-
+print("------------------")
+print("TOTAL PRICE: " + str(total_price))
+print("------------------")
+print("THANKS, COME AGAIN!")
 
 
 
